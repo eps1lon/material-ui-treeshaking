15cccda1cdfa704791410034a2e1a3509da50a6c
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2018-10-25 12:37:46 +0200

    Those increased somehow

--- .size-limit.js
+++ .size-limit.js
@@ -3,26 +3,26 @@ module.exports = [
     name: "documented Typography usage",
     webpack: false,
     path: "bundle/current-manual/index.current-manual.js",
-    limit: "55009 B"
+    limit: "55016 B"
   },
   {
     name: "documented Typography usage in evergreen browsers",
     webpack: false,
     path: "bundle/current-manual-es/index.current-manual-es.js",
-    limit: "53423 B"
+    limit: "53432 B"
   },
   {
     name: "documented Typography with custom webpack conf for tree shaking",
     webpack: false,
     path: "bundle/current-manual-shakeable/index.current-manual-shakeable.js",
-    limit: "53989 B"
+    limit: "55309 B"
   },
   {
     name: "Typography with tree shaking from webpack",
     webpack: false,
     path: "bundle/current-magic/index.current-magic.js",
     // little bit bigger because of named import overhead
-    limit: "55125 B"
+    limit: "55136 B"
   },
   {
     name: "next: Typography with tree shaking from webpack",
