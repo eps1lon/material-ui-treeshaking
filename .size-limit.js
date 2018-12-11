d439e92740752b7c55ffcc274c798c66ac697b22
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2018-12-11 11:00:41 +0100

    Update @material-ui/core

--- .size-limit.js
+++ .size-limit.js
@@ -3,26 +3,26 @@ module.exports = [
     name: "documented Typography usage",
     webpack: false,
     path: "bundle/current-manual/index.current-manual.js",
-    limit: "54029 B"
+    limit: "54807 B"
   },
   {
     name: "documented Typography usage in evergreen browsers",
     webpack: false,
     path: "bundle/current-manual-es/index.current-manual-es.js",
-    limit: "52797 B"
+    limit: "53146 B"
   },
   {
     name: "documented Typography with custom webpack conf for tree shaking",
     webpack: false,
     path: "bundle/current-manual-shakeable/index.current-manual-shakeable.js",
-    limit: "53340 B"
+    limit: "53754 B"
   },
   {
     name: "Typography with tree shaking from webpack",
     webpack: false,
     path: "bundle/current-magic/index.current-magic.js",
     // little bit bigger because of named import overhead
-    limit: "54149 B"
+    limit: "54928 B"
   },
   {
     name: "next: Typography with tree shaking from webpack",
