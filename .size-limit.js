faa156e444e8e19dc0112757c16dc2b052622910
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2018-12-14 11:36:00 +0100

    Fixup bundle size
    Fixes broken c41b158a1b7a9b1a31cecea828935f253f158954

--- .size-limit.js
+++ .size-limit.js
@@ -3,32 +3,32 @@ module.exports = [
     name: "documented Paper usage",
     webpack: false,
     path: "bundle/current-manual/index.current-manual.js",
-    limit: "19853 B"
+    limit: "69621 B"
   },
   {
     name: "documented Paper usage in evergreen browsers",
     webpack: false,
     path: "bundle/current-manual-es/index.current-manual-es.js",
-    limit: "18382 B"
+    limit: "68505 B"
   },
   {
     name: "documented Paper with custom webpack conf for tree shaking",
     webpack: false,
     path: "bundle/current-manual-shakeable/index.current-manual-shakeable.js",
-    limit: "18915 B"
+    limit: "69693 B"
   },
   {
     name: "Paper with tree shaking from webpack",
     webpack: false,
     path: "bundle/current-magic/index.current-magic.js",
     // little bit bigger because of named import overhead
-    limit: "19951 B"
+    limit: "71265 B"
   },
   {
     name: "next: Paper with tree shaking from webpack",
     webpack: false,
     path: "bundle/next-magic/index.next-magic.js",
     // gzipped tricked me :()
-    limit: "20049 B"
+    limit: "55040 B"
   }
 ];
